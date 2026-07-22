0b28ba159b3b901d5b341b01e6881475cbd271fc
Divi-more <dmore4018@gmail.com>
2026-07-22 14:08:30 +0530

    First commit

---
+++ demo.py
@@ -0,0 +1,60 @@
+# This is a simple program that takes user input and prints a greeting message.
+#name = input("enter your name: ")
+#print ("Hello " + name, "!")
+
+# This program takes two numbers as input from the user and prints their sum.
+# num1 = float(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# sum = num1 + num2
+# print("The sum is:", sum)
+
+# This program checks if a number is even or odd.
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(num, "is an even number.")
+# else:
+#     print(num, "is an odd number.")
+
+# This program checks if a number is positive, negative, or zero.
+# num = int(input("Enter a number: "))
+# if num > 0:
+#     print(num, "is a Positive number.")
+# elif num < 0:
+#     print(num, "is a Negative number.")
+# else:
+#     print(num, "is Zero.")
+
+# This program prints numbers from 1 to 5 using a for loop.
+# for i in range(1,6):
+    # print(i)
+
+# word = "Hello, World!"
+# for ch in word:
+#     print(ch)
+    
+# for i in range(len(word)):
+#     print("Position", i, ":", word[i])
+    
+# count = 0
+# for ch in word:
+#         count += 1
+# print("Number of letters in the word:", count)
+
+# This program counts the number of vowels in a given text.
+# text = input("Enter a text: ")
+# count = 0
+# for ch in text.lower():
+#     if ch in "aeiou":
+#         count += 1
+# print("Number of vowels in the text:", count)
+
+num = int(input("Enter a number: "))
+
+if num > 1 :
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number.")
+            break
+        else:
+            print(num, "is a prime number.")
+            break
